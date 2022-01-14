fe3d3fb2a54b9ac3fa3ba5668730771c85e64b7c
Matthew Reichardt <me@matthewreichardt.com>
2022-01-14 12:18:19 -0600

    Enhanced home page view 👓

--- app/views/pages/welcome.blade.php
+++ app/views/pages/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Welcome')
 
 @section('content')
-<div class="container countries">
+    <div class="container countries">
         <div class="row justify-content-md-center gy-4">
             <div class="col s12 hero">
                 <h1 class="text-center">We have intel on the biggest threats</h1>
@@ -13,14 +13,15 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container welcome-page countries">
         <div class="row list justify-content-md-center gy-4">
             <div class="col-12 blips">
-                <div class="border">
-                    <h1>Recent updates</h1>
+                <div class="border" style="border: 0px !important;">
                     <div class="row news front-page">
                         @foreach ($news as $news)
                             @if ($news['type'] == 'video')
-                                <a href="{{ $news['video_url'] }}" class="col-sm-6" style="margin-top: 0px;" target="_blank">
+                                <a href="{{ $news['video_url'] }}" class="col-sm-6 col-lg-4" style="margin-top: 0px;" target="_blank">
                                     <div class="video text-start">
                                         <i class="fab fa-youtube float-start"></i>
                                         <h4> {{ html_entity_decode($news['title'], ENT_QUOTES) }}</h4>
@@ -29,7 +30,7 @@
                                     </div>
                                 </a>
                             @elseif ($news['type'] == 'article')
-                                <a href="/{{ strtolower($news['country']) }}/{{ str_replace(' ', '_', $news['title']) }}" class="col-sm-6" style="margin-top: 0px;">
+                                <a href="/{{ strtolower($news['country']) }}/{{ str_replace(' ', '_', $news['title']) }}" class="col-sm-6 col-lg-4" style="margin-top: 0px;">
                                     <div class="article text-start">
                                         <h4>{{ html_entity_decode($news['title'], ENT_QUOTES) }}</h4>
                                         <h5>{{ html_entity_decode($news['description'], ENT_QUOTES) }}</h5>
@@ -37,7 +38,7 @@
                                     </div>
                                 </a>
                                 @elseif ($news['type'] == 'blip')
-                                <a href="/{{ strtolower($news['country']) }}/{{ str_replace(' ', '_', $news['title']) }}" class="col-sm-6" style="margin-top: 0px;">
+                                <a href="/{{ strtolower($news['country']) }}/{{ str_replace(' ', '_', $news['title']) }}" class="col-sm-6 col-lg-4" style="margin-top: 0px;">
                                     <div class="article text-start">
                                         <h4>{{ news['country'] }}</h4>
                                         <h5>{{ news['processed_html'] }}</h5>
