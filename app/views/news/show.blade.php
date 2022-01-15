3db748fed1b3d73a0b15235a94c710abe685f678
Matthew Reichardt <me@matthewreichardt.com>
2022-01-14 19:41:06 -0600

    Added breadcrumps

--- app/views/news/show.blade.php
+++ app/views/news/show.blade.php
@@ -6,7 +6,13 @@
     <div class="container countries">
         <div class="row">
             <div class="col-12 article">
-                <h6 class="back"><a href="/{{ strtolower($article['country']) }}"><i class="fas fa-long-arrow-alt-left"></i> Back</a></h6>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/">GLINT</a></li>
+                        <li class="breadcrumb-item"><a href="/{{ strtolower($article['country']) }}">{{ strtoupper($article['country']) }}</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">ARTICLE</li>
+                    </ol>
+                </nav>
                 <h1>{{ $article['title'] }}</h1>
                 <h3 class="no-bold">{{ html_entity_decode($article['description'], ENT_QUOTES) }}</h3>
                 <h6 class="date">{{ html_entity_decode($article['created_at']->format('Y-m-d'), ENT_QUOTES) }}</h6>
