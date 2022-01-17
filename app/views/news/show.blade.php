a8067476f7899007da644b6d6cd682459f3122c5
Matthew Reichardt <me@matthewreichardt.com>
2022-01-16 21:41:04 -0600

    Added share article section

--- app/views/news/show.blade.php
+++ app/views/news/show.blade.php
@@ -19,5 +19,18 @@
             </div>
             <div class="col-12 processed-html">{!! $article['processed_html'] !!}</div>
         </div>
+        <div class="row share">
+            <div class="col-12 text-center">
+                <h4>Get the word out</h4>
+                <p>Let your tribe know what's happening</p>
+            </div>
+            <div class="col-12">
+                <ul class="row options">
+                    <li class="col-4"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https://itsglint.com/{{ $article['country'] }}/{{ str_replace(' ', '_', $article['title']) }}"><i class="fab fa-facebook"></i></a></li>
+                    <li class="col-4"><a target="_blank" href="https://twitter.com/intent/tweet?text={{ strtoupper($article['country']) }}:%20{{ str_replace(' ', '%20', $article['description'])}}%0D%0Dhttps://itsglint.com/{{ $article['country'] }}/{{ str_replace(' ', '_', $article['title']) }}%0D%0D%23{{ $article['country'] }} %40glintnews"><i class="fab fa-twitter"></i></a></li>
+                    <li class="col-4"><a target="_blank" href="https://www.reddit.com/submit?url=https://itsglint.com/{{ $article['country'] }}/{{ str_replace(' ', '_', $article['title']) }}&title={{ $article['title'] }}"><i class="fab fa-reddit"></i></a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 @endsection
