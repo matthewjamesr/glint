ff3c60dd03b49688f9517dca44c49d3b8d1d3249
Matthew Reichardt <me@matthewreichardt.com>
2022-01-25 22:05:56 -0600

    Fixed previous migration

--- app/database/migrations/2019_11_18_133625_create_users.php
+++ app/database/migrations/2019_11_18_133625_create_users.php
@@ -15,7 +15,7 @@ class CreateUsers extends Database {
          	static::$capsule::schema()->create("users", function (Blueprint $table) {
                 $table->increments('id');
          		$table->string('username');
-         		$table->string('fullname');
+         		$table->string('fullname')->nullable();
          		$table->string('email')->unique();
          		$table->timestamp('email_verified_at')->nullable();
          		$table->string('password');
