a9cd7d99415cbab5cb5128a7604d44197000d115
Matthew Reichardt <me@matthewreichardt.com>
2022-01-27 21:46:25 -0600

    Removed user table seed

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run() : array
     {
         return [
-            UsersSeeder::class,
             CountriesSeeder::class,
             NewsSeeder::class
         ];
