6dc1171711420993055c43b9f570f7254646c138
Matthew Reichardt <me@matthewreichardt.com>
2022-01-27 21:42:25 -0600

    Added verify_code to users table

---
+++ app/database/migrations/2022_01_28_043612_add_verify_code_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Leaf\Database;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddVerifyCodeToUsers extends Database
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        static::$capsule::schema()->table("users", function (Blueprint $table) {
+            $table->string("verify_code");
+        });
+
+        // you can now build your migrations with schemas
+        // Schema::build(static::$capsule, dirname(__DIR__) . "/Schema/users.json");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        static::$capsule::schema()->dropIfExists("users");
+    }
+}
